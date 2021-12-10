43e118bd48760bc3cb17d23be9571883e081afdc
Novák Viktor <novk.viktor@gmail.com>
2021-12-10 20:31:32 +0100

    init Puzzle9

---
+++ src/main/java/com/adventofcode/day9/Puzzle9.java
@@ -0,0 +1,12 @@
+package com.adventofcode.day9;
+
+import java.util.List;
+
+import static com.adventofcode.helper.InputReaderUtil.createListFromFile;
+
+public class Puzzle9 {
+
+    public static void main(String[] args) {
+        List<String> inputs = createListFromFile("src/main/resources/puzzle_input_9_test.txt");
+    }
+}
